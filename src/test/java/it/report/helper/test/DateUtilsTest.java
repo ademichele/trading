4af440dde23923bad07adcd62bce7bce6029434a
ademichele <nailo25@gmail.com>
2018-10-04 22:44:28 +0200

    Add files via upload

---
+++ src/test/java/it/report/helper/test/DateUtilsTest.java
@@ -0,0 +1,26 @@
+package it.report.helper.test;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import it.report.helper.DateUtils;
+
+public class DateUtilsTest {
+	
+	//I did not test the other methods because they are simple methods of Calendar and SimpleDateFormat and I created them just to avoid repeating code
+	@Test
+	public void getNextWorkingDay(){
+		String today = "10-05-2018";
+		String currency = "EUR";
+		Date instructionDate = DateUtils.stringToDate(today);
+		Date settlementDate = DateUtils.getNextWorkingDay(instructionDate, currency);
+		String nextWorkingDay = DateUtils.dayName(DateUtils.dateToString(settlementDate));	
+		assertTrue(nextWorkingDay.equalsIgnoreCase("Monday"));
+	}
+	
+}
+
+
