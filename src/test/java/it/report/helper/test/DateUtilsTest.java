85de099d59d2b7318bcae75b965158252698f4e9
ademichele <nailo25@gmail.com>
2018-10-05 20:12:01 +0200

    Add files via upload

--- src/test/java/it/report/helper/test/DateUtilsTest.java
+++ src/test/java/it/report/helper/test/DateUtilsTest.java
@@ -12,7 +12,7 @@ public class DateUtilsTest {
 	
 	//I did not test the other methods because they are simple methods of Calendar and SimpleDateFormat and I created them just to avoid repeating code
 	@Test
-	public void getNextWorkingDay(){
+	public void getNextWorkingDay_EUR_shouldBeMonday(){
 		String today = "10-05-2018";
 		String currency = "EUR";
 		Date instructionDate = DateUtils.stringToDate(today);
@@ -20,7 +20,16 @@ public class DateUtilsTest {
 		String nextWorkingDay = DateUtils.dayName(DateUtils.dateToString(settlementDate));	
 		assertTrue(nextWorkingDay.equalsIgnoreCase("Monday"));
 	}
-	
+
+	@Test
+	public void getNextWorkingDay_AED_shouldBeSunday(){
+		String today = "10-05-2018";
+		String currency = "AED";
+		Date instructionDate = DateUtils.stringToDate(today);
+		Date settlementDate = DateUtils.getNextWorkingDay(instructionDate, currency);
+		String nextWorkingDay = DateUtils.dayName(DateUtils.dateToString(settlementDate));	
+		assertTrue(nextWorkingDay.equalsIgnoreCase("Sunday"));
+	}
 }
 
 
